ae502b0770b438956be510e58b7fb4ba8cd64ce2
anto-iorfida <antonioiorfida@hotmail.com>
2024-04-15 15:17:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script >
+import axios from 'axios';
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+    
+  },
+  data() {
+    return {
+      store
+    };
+  },
+  methods: {
+    getcardObjectFromApi() {
+      // Prende le carte dall'api e popola il main
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      .then((response) => {
+        store.cardObject = response.data.results;
+        console.log(response);
+      });
+    }
+  },
+  mounted() {
+    this.getcardObjectFromApi();
+  }
+
+}
+</script>
+
+<template>
+<AppHeader></AppHeader>
+</template>
+
+<style >
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name: "AppHeader",
+        data(){
+                return{
+
+                }
+            } 
+        }
+</script>
+
+<template>
+<h1>ciao</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
